9ed336b9fc856c7f28cc9c6ee4bdd19da062a09b
pdamico <phil.damico@yahoo.com>
2014-04-22 08:53:10 -0400

    Initital revision

---
+++ printAll.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo $1
+echo "Press any key to continue [$1] [$2]"
+read theDir
+python -E ./printCPools.py $1 | tee $2.pools.txt
+python -E ./printVars.py $1 | tee $2.vars.txt
+python -E ./printInitBlocks.py $1 | tee $2.init.txt
+python -E ./printJoins_04.py $1 | tee $2.joins.txt
+python -E ./printComplexJoins.py $1 | tee $2.complex.joins.txt
+python -E ./printLts.py $1 | tee $2.lts.txt
+
